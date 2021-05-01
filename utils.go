8b423680298e95cea62001a7199b51a35859b18b
bokket <3100563328@qq.com>
2021-05-01 12:37:23 +0800

    Use path format instead of dns

--- utils.go
+++ utils.go
@@ -288,8 +288,7 @@ func (s *Service) detectLocation(name string) (location string, err error) {
 	defer func() {
 		err = s.formatError("detect_location", err, "")
 	}()
-
-	//url := fmt.Sprintf("%s://%s.%s:%d", s.config.Protocol, name, s.config.Host, s.config.Port)
+	
 	url := fmt.Sprintf("%s://%s/%s", s.config.Protocol, "qingstor.com" , name )
 
 	r, err := s.client.Head(url)
